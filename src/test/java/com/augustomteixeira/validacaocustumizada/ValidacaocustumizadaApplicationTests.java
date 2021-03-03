4906f88aaf778cbafb4b2d4e12de8c0ff23da944
Marcelo <augustomnzteixeira@gmail.com>
2021-03-03 16:12:26 -0300

    first commit

---
+++ src/test/java/com/augustomteixeira/validacaocustumizada/ValidacaocustumizadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augustomteixeira.validacaocustumizada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidacaocustumizadaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
